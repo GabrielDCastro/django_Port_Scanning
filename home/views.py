5f9571ad82b554653ed7800620e8f8860e0f2697
GabrielDCastro <gabrielrangel12@gmail.com>
2020-05-14 16:38:59 -0300

    port scanner funcionando

--- home/views.py
+++ home/views.py
@@ -13,28 +13,20 @@ class homeView(TemplateView):
         return render(request, self.template_name, {'form': form})
 
     def post (self, request):
+        ports = [21, 22, 23, 25, 80, 135, 8080, 443, 3306]
+        portas_abertas = []
         form = HomeForm(request.POST)
         if form.is_valid():
+            ip = form.cleaned_data['post']
+            for port in ports:
+                client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                client.settimeout(0.2)  # tempo que vai testar cada conexão
+                code = client.connect_ex((ip, port))  # diz o site quer quer conectar e a porta
+                if code == 0:
+                    portas_abertas.append(port)
             text = form.cleaned_data['post']
-
-            args = {'form': form, 'text': text}
+            args = {'form': form, 'text': text, 'portas_abertas': portas_abertas}
             return render(request, self.template_name, args)
         else:
             return render(request, self.template_name, {'form': form})
 
-
-def port(requests):
-    ip = "google.com"
-    ports =[21, 22, 23, 25, 80, 135, 8080, 443, 3306] #21=FTP, 22=SSH, 23=TELNET, 25=SMTP, 3306=MSQL
-
-    for port in ports:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # AF_INET conecta no dominio da internet
-        #sock_stream expecifica que é o TCP
-        client.settimeout(0.2)#tempo que vai testar cada conexão
-        code = client.connect_ex((ip, port)) #diz o site quer quer conectar e a porta
-
-        if code == 0:
-            print ("{} -> porta aberta".format(port))
-
-    print ("Scan finalizado")
