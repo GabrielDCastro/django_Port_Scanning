af23fd8de7ad820d47969fd75843403f71fe46ec
GabrielDCastro <gabrielrangel12@gmail.com>
2020-05-22 23:02:51 -0300

    documentação de código e requirements

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
 class HomeForm(forms.Form):
-    post = forms.CharField()
+    #In HTML, a form is a collection of elements inside <form>...</form> that allow a visitor to do
+    # things like enter text, select options, manipulate objects or controls,
+    # and so on, and then send that information back to the server.
+    post = forms.CharField()#Criando um formulário de campo de texto
+
--- home/views.py
+++ home/views.py
@@ -4,22 +4,25 @@ from django.shortcuts import render
 from home.forms import HomeForm
 import socket
 
-
+#antes fazia as views só com funções, mas usar classe para facilitar, Então tanto faz.
 class homeView(TemplateView):
     template_name = 'home.html'
 
+    #função get e post têm que ter esse nome por padão do django
     def get (self, request):
-        form = HomeForm()
-        return render(request, self.template_name, {'form': form})
+        form = HomeForm() #criando um formulário vazio
+        return render(request, self.template_name, {'form': form}) #retornando a página html e o formulário criado
 
     def post (self, request):
-        ports = [21, 22, 23, 25, 80, 135, 8080, 443, 3306]
+        ports = [21, 22, 23, 25, 80, 135, 8080, 443, 3306]#algumas portas importantes 21=FTP, 22=SSH, 23=TELNET, 25=SMTP, 3306=MSQL
         portas_abertas = []
-        form = HomeForm(request.POST)
+        form = HomeForm(request.POST)#método pra quando o formulário for requisitado
         try:
             form.is_valid()
-            ip = form.cleaned_data['post']
+            ip = form.cleaned_data['post']#deixando a variável igual ao que a pessoa escreveu em forma de string
             for port in ports:
+                # AF_INET conecta no dominio da internet
+                # sock_stream expecifica que é o TCP
                 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client.settimeout(0.2)  # tempo que vai testar cada conexão
                 code = client.connect_ex((ip, port))  # diz o site quer quer conectar e a porta
